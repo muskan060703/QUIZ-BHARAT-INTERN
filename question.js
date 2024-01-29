0007e166db294547b07266024d5b08acddd98689
MUSKAN <150511701+muskan060703@users.noreply.github.com>
2024-01-29 22:26:26 +0530

    Add files via upload

---
+++ question.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "HTML stands for",
+        answer: "C. HyperText Markup Language",
+        options: [
+                "A. HighText Machine Language",
+                "B. HyperText and links Markup Language",
+                "C. HyperText Markup Language",
+                "D. None of these"
+        ]
+    },
+    {
+        numb: 2,
+        question: "The correct sequence of HTML tags for starting a webpage is -",
+        answer: "C. HTML, Head, Title, Body",
+        options: [
+            "A. Head, Title, HTML, body",
+            "B. HTML, Body, Title, Head",
+            "C. HTML, Head, Title, Body",
+            "D. HTML, Head, Body, Title"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which of the following programming languages is commonly used for server-side scripting in web development?",
+        answer: "D. PHP",
+        options: [
+            "A. HTML",
+            "B. CSS",
+            "C. JavaScript",
+            "D. PHP"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which of the following is a popular front-end framework for building user interfaces in JavaScript?",
+        answer: "B. Angular",
+        options: [
+            "A. Django",
+            "B. Angular",
+            "C. Flask",
+            "D. Node.js"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What is DOM?",
+        answer: "C. Convention for representing and interacting with objects in html documents",
+        options: [
+            "A. Application programming interface",
+            "B. Hierarchy of objects in ASP.NET",
+            "C. Convention for representing and interacting with objects in html documents",
+            "D.  None of the mentioned"
+        ]
+    }
+];
